ade84ca9482524d284c2ffb17a89dbd121566cec
moustafa sawan <moustafa.sawan@gmail.com>
2023-10-19 19:44:22 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ostim.Models;
+
+namespace Ostim.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private List<Employee> _employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 0, Name = "Martin", Surname = "Simpson",
+                BirthDate = new DateTime(1992, 12, 3),
+                Position = "Marketing Expert", Image="/images/Martin.jpg"
+            },
+            new Employee
+            {
+                Id = 1, Name = "Jacob", Surname = "Hawk",
+                BirthDate = new DateTime(1995, 10, 2), Position = "Manager", Image="/images/Jacob.jpg"
+            },
+            new Employee
+            {
+                Id = 2, Name = "Elizabeth", Surname = "Geil",
+                BirthDate = new DateTime(2000, 1, 7),
+                Position = "Software Engineer", Image="/images/Elizabeth.jpg"
+            },
+            new Employee
+            {
+                Id = 3, Name = "Kate", Surname =  "Metain",
+                BirthDate = new DateTime(1997, 2, 13),
+                Position = "Admin", Image="/images/Kate.jpg"
+            },
+            new Employee
+            {
+                Id = 4, Name = "Michael", Surname = "Cook",
+                BirthDate = new DateTime(1990, 12, 25),
+                Position = "Marketing expert", Image="/images/Michael.jpg"
+            },
+            new Employee
+            {
+                Id = 5, Name = "John", Surname = "Snow",
+                BirthDate = new DateTime(2001, 7, 15),
+                Position = "Software Engineer", Image="/images/John.jpg"
+            },
+            new Employee
+            {
+                Id = 6, Name = "Nina", Surname = "Soprano",
+                BirthDate = new DateTime(1999, 9, 30),
+                Position = "Software Engineer", Image="/images/Nina.jpg"
+            },
+            new Employee
+            {
+                Id = 7, Name = "Tina", Surname = "Fins",
+                BirthDate = new DateTime(2000, 5, 14),
+                Position = "Team Leader", Image="/images/Tina.jpg"
+            }
+        };
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            return View(_employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee employee = _employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return HttpNotFound(); // Return a 404 error
+            }
+            return View(employee);
+        }
+
+        private ActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ostim.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        public string Position { get; set; }
+        public string Image { get; set; }
+
+
+    }
+}
